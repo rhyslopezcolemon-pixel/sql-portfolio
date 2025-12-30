61c4f432fc4698bba6ccfc0451a3e26acddf73bc
rhyslopezcolemon-pixel <rhyslopezcolemon@gmail.com>
2025-12-30 15:41:53 -0500

    Update analysis_queries.sql

--- servicenow-ticket-analysis/analysis_queries.sql
+++ servicenow-ticket-analysis/analysis_queries.sql
@@ -1,1 +1,16 @@
+/* 
+Question 1:
+How many incidents were logged per day?
+
+Purpose:
+Establish baseline daily workload and identify natural variation
+in virtual support ticket volume.
+*/
+
+SELECT
+    contact_date,
+    COUNT(incident_number) AS daily_incident_count
+FROM incidents
+GROUP BY contact_date
+ORDER BY contact_date;
 
