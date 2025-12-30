4d863b0c62dadd969a19d1406aeaf51da13cce93
rhyslopezcolemon-pixel <rhyslopezcolemon@gmail.com>
2025-12-30 15:40:05 -0500

    Update schema.sql

--- servicenow-ticket-analysis/schema.sql
+++ servicenow-ticket-analysis/schema.sql
@@ -1,1 +1,19 @@
+-- Incidents table
+CREATE TABLE incidents (
+    incident_number VARCHAR(10) PRIMARY KEY,
+    contact_date DATE,
+    nuid VARCHAR(7),
+    asset_management VARCHAR(9),
+    resolved_status CHAR(2)
+);
+
+-- Users table
+CREATE TABLE users (
+    nuid VARCHAR(7) PRIMARY KEY
+);
+
+-- Assets table
+CREATE TABLE assets (
+    asset_management VARCHAR(9) PRIMARY KEY
+);
 
